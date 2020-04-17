5839dbf0c914517db11d81881a3a6ffeb9f85b88
Maciej <maciej.helak@gmail.com>
2020-04-17 17:16:02 +0200

    ApplicationProperties

--- ZamarinKurs/ZamarinKurs/App.xaml.cs
+++ ZamarinKurs/ZamarinKurs/App.xaml.cs
@@ -7,15 +7,20 @@ namespace ZamarinKurs
 {
     public partial class App : Application
     {
+
+        private const string TitleKey = "Name";
+        private const string NotificationsEnabledKey = "NotificationsEnabled";
         public App()
         {
             InitializeComponent();
 
             //MainPage = new TableViewPage();
 
+            MainPage = new ApplicationPropertiesPage();
+
             //MainPage = new NavigationPage ( new ToolbarMainPage());
 
-            MainPage = new NavigationPage(new TableViewPage());
+            //MainPage = new NavigationPage(new TableViewPage());
 
 
             //MainPage = new NavigationPage (new ContactsPage());
@@ -42,6 +47,34 @@ namespace ZamarinKurs
         protected override void OnResume()
         {
             // Handle when your app resumes
+        }
+
+        public string Title
+        {
+            get
+            {
+                if (Properties.ContainsKey(TitleKey))
+                   return Properties[TitleKey].ToString();
+                return "";
+            }
+            set
+            {
+                Properties[TitleKey] = value;
+            }
+        }
+
+        public bool NotificationsEnabled
+        {
+            get
+            {
+                if (Properties.ContainsKey(NotificationsEnabledKey))
+                   return (bool)Properties[NotificationsEnabledKey];
+                return false;
+            }
+            set
+            {
+                Properties[NotificationsEnabledKey] = value;
+            }
         }
     }
 }
---
+++ ZamarinKurs/ZamarinKurs/ApplicationPropertiesPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ZamarinKurs
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ApplicationPropertiesPage : ContentPage
+    {
+        //przeniesiono do App.xaml.cs
+        //private const string TitleKey = "Name";
+        //private const string NotificationsEnabledKey = "NotificationsEnabled";
+
+        public ApplicationPropertiesPage()
+        {
+            InitializeComponent();
+
+            // przed przenisieniem zmiennych do App
+            //if (Application.Current.Properties.ContainsKey(TitleKey))
+            //    title.Text = Application.Current.Properties[TitleKey].ToString();
+
+            //if (Application.Current.Properties.ContainsKey(NotificationsEnabledKey))
+            //    notificationsEnabled.On = (bool)Application.Current.Properties[NotificationsEnabledKey];
+
+            //po przeniesieniu zmiennych do App
+
+            BindingContext = Application.Current;
+
+            //po dodaniu  BindingContext
+            //var app = Application.Current as App;
+            //title.Text = app.Title;
+            //notificationsEnabled.On = app.NotificationsEnabled;
+        }
+
+
+        // przed przenisieniem zmiennych do App
+        //private void OnChange(object sender, EventArgs e)
+        //{
+        //    Application.Current.Properties[TitleKey] = title.Text;
+        //    Application.Current.Properties[NotificationsEnabledKey] = notificationsEnabled.On;
+
+        //    //Application.Current.SavePropertiesAsync(); //zapisuje ustawienia w locie
+        //}
+
+
+        //po przeniesieniu zmiennych do App
+        //po dodaniu  BindingContext nie potrzebna metoda
+        //private void OnChange(object sender, EventArgs e)
+        //{
+        //    var app = Application.Current as App;
+        //    app.Title = title.Text;
+        //    app.NotificationsEnabled = notificationsEnabled.On;
+        //}
+    }
+}
